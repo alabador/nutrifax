182b5875df847412162888bc898bcd3aaad5aa79
Austin Labador <austinlabador@gmail.com>
2023-05-25 03:35:00 -0700

    Setup fetch request to api and receive data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+
 function App() {
+  const [food, setFood] = useState<string>('apple')
+  const [result, setResult] = useState([])
   
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch(`https://trackapi.nutritionix.com/v2/search/instant?query=${food}`, {
+        headers: {
+          'x-app-id': `${import.meta.env.VITE_APP_ID}`,
+          'x-app-key': `${import.meta.env.VITE_APP_KEY}`
+        },
+      })
+      const data = await res.json()
+      console.log(data)
+      setResult(data)
+    }
+    getData()
+  }, [])
 
   return (
     <>
