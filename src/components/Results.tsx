2d7a367cd5a80e9c1856b440df667b16512e8711
Austin Labador <austinlabador@gmail.com>
2023-05-29 14:51:40 -0700

    Add card render for empty state

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,4 +1,4 @@
-import { Container} from "@chakra-ui/react";
+import { Container, Card, CardBody, Image, Text} from "@chakra-ui/react";
 import ResultCard from "./subcomponents/ResultCard";
 import { Common } from "../App";
 
@@ -19,7 +19,17 @@ export default function Results(props:Results) {
                     image={result.photo.thumb} 
                 />
                 )
-            }) : <h2>Empty</h2>}
+            }) : (
+                <Card direction={"row"} align={"center"}>
+                    <CardBody >
+                        <Text>No results found.</Text>
+                    </CardBody>
+                    <Image 
+                        w={"200px"} 
+                        src="https://images.unsplash.com/photo-1539136788836-5699e78bfc75?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" 
+                    />
+                </Card>
+            )}
         </Container>
     )
 }
