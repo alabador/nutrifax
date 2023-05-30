9387e7d1653c090c9437cb55b688fdbb5438ccc4
Austin Labador <austinlabador@gmail.com>
2023-05-30 14:07:57 -0700

    Implement fetch details for food, console log for now

--- src/components/subcomponents/InfoModal.tsx
+++ src/components/subcomponents/InfoModal.tsx
@@ -1,4 +1,5 @@
 import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 interface ModalProps {
     foodName: string,
@@ -9,7 +10,26 @@ interface ModalProps {
 
 
 export default function InfoModal(props:ModalProps) {  
+
+    const [details, setDetails] = useState([])
     
+    useEffect(() => {
+        async function getDetails(data = {query:props.foodName}) {
+            const res = await fetch('https://trackapi.nutritionix.com/v2/natural/nutrients' , {
+                method: "POST",
+                headers: {
+                  'x-app-id': `${import.meta.env.VITE_APP_ID}`,
+                  'x-app-key': `${import.meta.env.VITE_APP_KEY}`,
+                  'Content-Type' : 'application/json'
+                },
+                body: JSON.stringify(data)
+              })
+            const responseData = await res.json()
+            setDetails(responseData)
+            console.log(details)
+        }
+        getDetails()
+    }, [props.isOpen])
 
     return(
         <>
