4c3b78d9bb7b384b741e8cb3757cd7ae58f90cbf
Austin Labador <austinlabador@gmail.com>
2023-05-29 22:26:41 -0700

    Break down result card into modal subcomponent

--- src/components/subcomponents/InfoModal.tsx
+++ src/components/subcomponents/InfoModal.tsx
@@ -1,7 +1,31 @@
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text } from "@chakra-ui/react";
+
+interface ModalProps {
+    foodName: string,
+    isOpen: boolean,
+    onOpen: () => void,
+    onClose: () => void
+}
 
 
-export default function InfoModal() {  
+export default function InfoModal(props:ModalProps) {  
+    
+
     return(
-        <h2>Test</h2>
+        <>
+            <Modal isOpen={props.isOpen} onClose={props.onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>{props.foodName}</ModalHeader>
+                    <ModalBody>
+                        <Text>test text</Text>
+                    </ModalBody>
+                    <ModalFooter gap={2}>
+                        <Button onClick={props.onClose}>Close</Button>
+                        <Button colorScheme="green">Add to Calorie Count</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
--- src/components/subcomponents/ResultCard.tsx
+++ src/components/subcomponents/ResultCard.tsx
@@ -1,5 +1,6 @@
 import { Button, Card, CardBody, Flex, Image, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
 import './ResultCard.css'
+import InfoModal from "./InfoModal";
 
 interface CardProps {
     foodName: string,
@@ -9,6 +10,7 @@ interface CardProps {
 export default function ResultCard({foodName, image}:CardProps) {  
 
     const {isOpen, onOpen, onClose} = useDisclosure();
+
 
     return (
         <>
@@ -24,19 +26,12 @@ export default function ResultCard({foodName, image}:CardProps) {
                     </Flex>
                 </CardBody>
             </Card>
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>{foodName}</ModalHeader>
-                    <ModalBody>
-                        <Text>test text</Text>
-                    </ModalBody>
-                    <ModalFooter gap={2}>
-                        <Button onClick={onClose}>Close</Button>
-                        <Button colorScheme="green">Add to Calorie Count</Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
+            <InfoModal 
+                isOpen={isOpen} 
+                onOpen={onOpen} 
+                onClose={onClose} 
+                foodName={foodName}
+            />
         </>
     )
 }
