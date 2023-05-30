5bb449c6c2fcf3eaaa142a5843ffc40cf0fab411
Austin Labador <austinlabador@gmail.com>
2023-05-29 22:03:07 -0700

    Add modal and hover state to card

--- src/components/subcomponents/ResultCard.tsx
+++ src/components/subcomponents/ResultCard.tsx
@@ -1,23 +1,42 @@
-import { Card, CardBody, Flex, Image, Text } from "@chakra-ui/react";
+import { Button, Card, CardBody, Flex, Image, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
+import './ResultCard.css'
 
 interface CardProps {
     foodName: string,
     image: string,
 }
 
-export default function ResultCard({foodName, image}:CardProps) {
+export default function ResultCard({foodName, image}:CardProps) {  
+
+    const {isOpen, onOpen, onClose} = useDisclosure();
+
     return (
-        <Card mb="8px">
-            <CardBody>
-                <Flex justify="space-between" align="center">
-                    <Text>{foodName}</Text>
-                    <Image
-                        src={image}
-                        borderRadius='lg'
-                        height="30px"
-                    />
-                </Flex>
-            </CardBody>
-        </Card>
+        <>
+            <Card mb="8px" className="resultCard" onClick={onOpen}>
+                <CardBody >
+                    <Flex justify="space-between" align="center" >
+                        <Text>{foodName}</Text>
+                        <Image
+                            src={image}
+                            borderRadius='lg'
+                            height="30px"
+                        />
+                    </Flex>
+                </CardBody>
+            </Card>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>{foodName}</ModalHeader>
+                    <ModalBody>
+                        <Text>test text</Text>
+                    </ModalBody>
+                    <ModalFooter gap={2}>
+                        <Button onClick={onClose}>Close</Button>
+                        <Button colorScheme="green">Add to Calorie Count</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+        </>
     )
 }
